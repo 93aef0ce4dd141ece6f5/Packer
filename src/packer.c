86a4602e95a7806fa18755487616e1ab83d38eb2
93aef0ce4dd141ece6f5 <asdfhui@gmail.com>
2016-08-01 22:05:47 +1000

    Added GNU GPL license

--- src/packer.c
+++ src/packer.c
@@ -1,3 +1,23 @@
+/*
+
+    Compresses and encrypts an executable payload
+    Copyright (C) 2016  93aef0ce4dd141ece6f5
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <windows.h>
@@ -229,6 +249,7 @@ BOOL CompressPayload(FileStruct *fs) {
 }
 
 int main(int argc, char *argv[]) {
+	printf("Copyright (C) 2016  93aef0ce4dd141ece6f5\n\n");
 	if (argc < 3) {
 		Debug("Usage: %s [INPUT FILE] [OUTPUT FILE]\n", argv[0]);
 		return 1;
